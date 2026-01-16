743c53386f8088ffe9b92e7cb6110431326fa215
Aisosa2015 <godwinlilian02@gmail.com>
2026-01-16 09:36:34 +0100

    my task

---
+++ DataCollection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
